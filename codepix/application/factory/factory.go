4fb3fea5c58f1f1ad296b2df93bed174d11cf0c1
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-07 20:39:08 -0300

    feat(codepix): Apache Kafka implementation

---
+++ codepix/application/factory/factory.go
@@ -0,0 +1,19 @@
+package factory
+
+import (
+	"github.com/iamtheluiz/codepix/codepix-go/application/usecase"
+	"github.com/iamtheluiz/codepix/codepix-go/infrastructure/repository"
+	"github.com/jinzhu/gorm"
+)
+
+func TransactionUseCaseFactory(database *gorm.DB) usecase.TransactionUseCase {
+	pixRepository := repository.PixKeyRepositoryDb{Db: database}
+	transactionRepository := repository.TransactionRepositoryDb{Db: database}
+
+	transactionUseCase := usecase.TransactionUseCase{
+		TransactionRepository: &transactionRepository,
+		PixRepository:         pixRepository,
+	}
+
+	return transactionUseCase
+}
